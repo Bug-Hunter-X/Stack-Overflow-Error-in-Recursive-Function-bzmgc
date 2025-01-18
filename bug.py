9b8209dc05ac00bec93b4fd0d35b5367b985c2d2
BugHunter-X <askzenai@gmail.com>
2025-01-18 09:28:04 +0100

    Add bug.py file

---
+++ bug.py
@@ -0,0 +1,7 @@
+def function_with_uncommon_error(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return function_with_uncommon_error(n - 1) + function_with_uncommon_error(n - 2)
